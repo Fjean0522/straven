a615836b4ba81c65d7a67e3f22d19dcf19019282
Fjean <fjean0522@gmail.com>
2023-09-21 18:12:07 -0400

    Create play button for hero.

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,10 +1,24 @@
-
+import play_btn from '../assets/play_btn.png'
 
 const Hero = () => {
   return (
-    <>
-      
-    </>
+    <div>
+      <h2 className='text-white font-bold'>Detective Reacher</h2>
+      <p className='text-white'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Ut a sapien sed ex feugiat faucibus nec quis lectus. 
+        Maecenas facilisis pretium diam, a sodales risus malesuada eu.
+      </p>
+      <div 
+        className='bg-white flex items-center justify-center gap-2 text-lg font-bold w-28 p-2 rounded-xl'>
+        <img
+          src={play_btn} 
+          alt="Play Button" 
+          className='w-6'
+        />
+        Play
+      </div>
+    </div>
   )
 }
 
