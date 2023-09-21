b1571edc71b07646e5bc653650cd23e8fb90239a
Fjean <fjean0522@gmail.com>
2023-09-20 20:27:55 -0400

    Style & add icons to header.
    Also import components to Home page.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,38 @@
+import logo from '../assets/straven_logo.png'
+import menu from '../assets/hamburger_menu.png'
+import search from '../assets/search.png'
+import settings from '../assets/settings.png'
 
 
 const Header = () => {
   return (
     <>
-      
+      <nav className='flex justify-between items-center p-5'>
+        <div className='flex items-center gap-5'>
+          <h1 className='text-white text-3xl font-bold'>
+            <span className='text-blue-600'>Str</span>
+            aven
+          </h1>
+          <img 
+            src={menu} 
+            alt='Menu'
+            className='w-8 h-8'
+          />
+        </div>
+
+        <div className='flex items-center gap-2'>
+          <img
+            src={search} 
+            alt='Search Icon'
+            className='w-10 h-10'
+          />
+          <img 
+            src={settings} 
+            alt='Settings'
+            className='w-10 h-10'
+          />
+        </div>
+      </nav>
     </>
   )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,16 @@
-
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import CategorySelection from "../components/CategorySelection"
+import ContentDisplay from "../components/ContentDisplay"
 
 const Home = () => {
   return (
-    <>
-      
-    </>
+    <div className="bg-black min-h-screen">
+      <Header />
+      <Hero />
+      <CategorySelection />
+      <ContentDisplay />
+    </div>
   )
 }
 
