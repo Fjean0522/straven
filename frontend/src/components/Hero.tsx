425a68fcc762e89c5d0c62e2ab9cbcce0ae59a68
Fjean <fjean0522@gmail.com>
2023-09-26 12:50:15 -0400

    Add placeholder movie thumbnail to hero section.

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,10 +1,18 @@
 import play_btn from '../assets/play_btn.png'
+import movieThumbnail from '../assets/detective_reacher_placeholder.png'
 
 const Hero = () => {
+  const  heroStyles = {
+    backgroundImage: `url(${movieThumbnail})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  }
+
   return (
-    <div>
+    <div className='p-6 h-72 md:h-96' style={heroStyles}>
       <h2 className='text-white font-bold'>Detective Reacher</h2>
-      <p className='text-white'>
+      <p className='text-white mb-3'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Ut a sapien sed ex feugiat faucibus nec quis lectus. 
         Maecenas facilisis pretium diam, a sodales risus malesuada eu.
