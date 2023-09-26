269fba0e4e600d23feede465993da19b00964bfe
Fjean <fjean0522@gmail.com>
2023-09-26 14:38:11 -0400

    Make hero section responsive for larger screens.

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -10,21 +10,27 @@ const Hero = () => {
   }
 
   return (
-    <div className='p-6 h-72 md:h-96' style={heroStyles}>
-      <h2 className='text-white font-bold'>Detective Reacher</h2>
-      <p className='text-white mb-3'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Ut a sapien sed ex feugiat faucibus nec quis lectus. 
-        Maecenas facilisis pretium diam, a sodales risus malesuada eu.
-      </p>
-      <div 
-        className='bg-white flex items-center justify-center gap-2 text-lg font-bold w-28 p-2 rounded-xl'>
-        <img
-          src={play_btn} 
-          alt="Play Button" 
-          className='w-6'
-        />
-        Play
+    <div className='p-6 h-72 md:h-96' 
+         style={heroStyles}
+    >
+      <div className='flex flex-col justify-center h-full lg:ml-3'>
+        <div className='max-w-xs md:max-w-md lg:max-w-lg'>
+          <h2 className='text-white font-bold'>Detective Reacher</h2>
+          <p className='text-white mb-3'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Ut a sapien sed ex feugiat faucibus nec quis lectus. 
+            Maecenas facilisis pretium diam, a sodales risus malesuada eu.
+          </p>
+        </div>
+
+        <div className='bg-white flex items-center justify-center gap-2 text-lg font-bold w-28 p-2 rounded-xl'>
+          <img
+            src={play_btn} 
+            alt="Play Button" 
+            className='w-6'
+          />
+          Play
+        </div>
       </div>
     </div>
   )
