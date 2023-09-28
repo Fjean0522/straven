c0bc92b1dd3e5b3552c6b14da33880193d86ceb5
Fjean <fjean0522@gmail.com>
2023-09-28 12:34:23 -0400

    Add 'cursor pointer' to logo.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     <>
       <nav className='sticky top-0 z-50 bg-black bg-opacity-50 flex justify-between items-center p-5 md:px-10 lg:px-14'>
         <div className='flex items-center gap-5'>
-          <h1 className='text-white text-3xl font-bold'>
+          <h1 className='text-white text-3xl font-bold cursor-pointer'>
             <span className='text-blue-600'>Str</span>
             aven
           </h1>
