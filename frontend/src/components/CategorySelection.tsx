6e96589bdf922cc75b75abccd2e48662a2f2907b
Fjean <fjean0522@gmail.com>
2023-09-28 12:28:35 -0400

    Add & style content in category selection section.

--- frontend/src/components/CategorySelection.tsx
+++ frontend/src/components/CategorySelection.tsx
@@ -2,10 +2,16 @@
 
 
 const CategorySelection = () => {
+
+
+  
   return (
-    <>
-      
-    </>
+    <div className="text-white font-bold flex justify-between p-6 md:justify-evenly md:text-lg">
+      <h2 className="hover:text-blue-600 cursor-pointer">Action</h2>
+      <h2 className="hover:text-blue-600 cursor-pointer">Comedy</h2>
+      <h2 className="hover:text-blue-600 cursor-pointer">Thriller</h2>
+      <h2 className="hover:text-blue-600 cursor-pointer">Horror</h2>
+    </div>
   )
 }
 
