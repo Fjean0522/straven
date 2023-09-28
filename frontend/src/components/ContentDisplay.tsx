744cf2208e75c1da01f154d12bda1c51c71d01f7
Fjean <fjean0522@gmail.com>
2023-09-28 15:01:24 -0400

    Link media contents to play page.

--- frontend/src/components/ContentDisplay.tsx
+++ frontend/src/components/ContentDisplay.tsx
@@ -1,6 +1,7 @@
 import movie_1 from '../assets/movie_1.png'
 import movie_2 from '../assets/movie_2.png'
 import movie_3 from '../assets/movie_3.png' 
+import { Link } from 'react-router-dom'
 
 const ContentDisplay = () => {
 
@@ -8,27 +9,33 @@ const ContentDisplay = () => {
   return (
     <div className="text-white font-semibold text-base flex justify-evenly gap-4 p-2 md:px-7">
       <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
-        <img 
-          src={movie_1} 
-          alt="Movie Thumbnail" 
-        />
-        <h1>Prism</h1>
+        <Link to='/play'>
+          <img 
+            src={movie_1} 
+            alt="Movie Thumbnail" 
+          />
+          <h1>Prism</h1>
+        </Link>
       </div>
 
       <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
-        <img 
-          src={movie_2} 
-          alt="Movie Thumbnail" 
-        />
-        <h1>Forever Abyss</h1>
+        <Link to='/play'>
+          <img 
+            src={movie_2} 
+            alt="Movie Thumbnail" 
+          />
+          <h1>Forever Abyss</h1>
+        </Link>
       </div>
 
       <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
-        <img 
-          src={movie_3} 
-          alt="Movie Thumbnail" 
-        />
-        <h1>The Haunting</h1>
+        <Link to='/play'>
+          <img 
+            src={movie_3} 
+            alt="Movie Thumbnail" 
+          />
+          <h1>The Haunting</h1>
+        </Link>
       </div>
     </div>
   )
