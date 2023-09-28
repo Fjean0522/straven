2c06dfde2afbef0ef10a88862d0ffc9b486ca917
Fjean <fjean0522@gmail.com>
2023-09-28 13:38:03 -0400

    Define media content section with dummy data.

--- frontend/src/components/CategorySelection.tsx
+++ frontend/src/components/CategorySelection.tsx
@@ -6,7 +6,7 @@ const CategorySelection = () => {
 
   
   return (
-    <div className="text-white font-bold flex justify-between p-6 md:justify-evenly md:text-lg">
+    <div className="text-white font-bold flex justify-between p-6 md:justify-evenly md:text-lg md:mb-5">
       <h2 className="hover:text-blue-600 cursor-pointer">Action</h2>
       <h2 className="hover:text-blue-600 cursor-pointer">Comedy</h2>
       <h2 className="hover:text-blue-600 cursor-pointer">Thriller</h2>
--- frontend/src/components/ContentDisplay.tsx
+++ frontend/src/components/ContentDisplay.tsx
@@ -1,10 +1,36 @@
-
+import movie_1 from '../assets/movie_1.png'
+import movie_2 from '../assets/movie_2.png'
+import movie_3 from '../assets/movie_3.png' 
 
 const ContentDisplay = () => {
+
+
   return (
-    <>
-      
-    </>
+    <div className="text-white font-semibold text-base flex justify-evenly gap-4 p-2 md:px-7">
+      <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
+        <img 
+          src={movie_1} 
+          alt="Movie Thumbnail" 
+        />
+        <h1>Prism</h1>
+      </div>
+
+      <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
+        <img 
+          src={movie_2} 
+          alt="Movie Thumbnail" 
+        />
+        <h1>Forever Abyss</h1>
+      </div>
+
+      <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
+        <img 
+          src={movie_3} 
+          alt="Movie Thumbnail" 
+        />
+        <h1>The Haunting</h1>
+      </div>
+    </div>
   )
 }
 
