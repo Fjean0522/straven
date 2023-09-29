9e8d3ee23ae0f4071ec5f2fe3614897471732b11
Fjean <fjean0522@gmail.com>
2023-09-29 14:38:53 -0400

    Add controls to media player.

--- frontend/src/components/MediaPlayer.tsx
+++ frontend/src/components/MediaPlayer.tsx
@@ -8,17 +8,20 @@ type MediaPlayerProp = {
 const MediaPlayer: React.FC<MediaPlayerProp> = ({ url }) => {
 
   return (
-    <div className="bg-black h-screen">
-      <div className="h-fit">
+    <div className="bg- h-screen">
+      <div className="h-fit pb-3">
         <ReactPlayer
+          controls
           url={url}
           width='100%'
           height='100%'
         />
       </div>
 
-      <div>
-
+      <div className="text-black flex gap-3">
+        <h2 className="text-2xl font-semibold">Prism</h2>
+        <p>PG-13</p>
+        <p>1h 37m</p>
       </div>
     </div>
   )
