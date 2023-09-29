22a6d42d9776f2e58ccafb8cc449ce993ad7086c
Fjean <fjean0522@gmail.com>
2023-09-29 11:37:09 -0400

    Change content display divs to link tags.

--- frontend/src/components/ContentDisplay.tsx
+++ frontend/src/components/ContentDisplay.tsx
@@ -8,35 +8,35 @@ const ContentDisplay = () => {
 
   return (
     <div className="text-white font-semibold text-base flex justify-evenly gap-4 p-2 md:px-7">
-      <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
-        <Link to='/play'>
-          <img 
-            src={movie_1} 
-            alt="Movie Thumbnail" 
-          />
-          <h1>Prism</h1>
-        </Link>
-      </div>
+      <Link 
+        to='/play'
+        className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
+        <img 
+          src={movie_1} 
+          alt="Movie Thumbnail" 
+        />
+        <h1>Prism</h1>
+      </Link>
 
-      <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
-        <Link to='/play'>
-          <img 
-            src={movie_2} 
-            alt="Movie Thumbnail" 
-          />
-          <h1>Forever Abyss</h1>
-        </Link>
-      </div>
+      <Link 
+        to='/play' 
+        className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
+        <img 
+          src={movie_2} 
+          alt="Movie Thumbnail" 
+        />
+        <h1>Forever Abyss</h1>
+      </Link>
 
-      <div className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
-        <Link to='/play'>
-          <img 
-            src={movie_3} 
-            alt="Movie Thumbnail" 
-          />
-          <h1>The Haunting</h1>
-        </Link>
-      </div>
+      <Link 
+        to='/play'
+        className='flex flex-col items-center gap-2 hover:scale-105 cursor-pointer'>
+        <img 
+          src={movie_3} 
+          alt="Movie Thumbnail" 
+        />
+        <h1>The Haunting</h1>
+      </Link>
     </div>
   )
 }
