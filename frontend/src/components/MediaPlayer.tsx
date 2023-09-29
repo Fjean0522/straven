86c6874a7c864951c32c6e84c4dd23c8cc5174e6
Fjean <fjean0522@gmail.com>
2023-09-29 14:30:44 -0400

    Create media player & import to play page.

--- frontend/src/components/MediaPlayer.tsx
+++ frontend/src/components/MediaPlayer.tsx
@@ -2,25 +2,25 @@ import ReactPlayer from "react-player"
 
 
 type MediaPlayerProp = {
-url: string
+  url: string
 }
 
 const MediaPlayer: React.FC<MediaPlayerProp> = ({ url }) => {
 
-  <div>
-    <ReactPlayer
-      url={url}
-      width='100%'
-      height='100%'
-      style={{ position: 'absolute', top: 0, left: 0 }}
-    />
-  </div>
-
-
   return (
-    <>
-      
-    </>
+    <div className="bg-black h-screen">
+      <div className="h-fit">
+        <ReactPlayer
+          url={url}
+          width='100%'
+          height='100%'
+        />
+      </div>
+
+      <div>
+
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/pages/Play.tsx
+++ frontend/src/components/pages/Play.tsx
@@ -2,11 +2,15 @@ import Header from "../Header"
 import MediaPlayer from '../MediaPlayer'
 
 const Play: React.FC = () => {
-  
+
+  const mediaUrl = '/src/assets/the_nun_2.mp4'
+
+  console.log(`Media is displaying with url: ${mediaUrl}`)
+
   return (
     <>
       <Header />
-      <MediaPlayer />
+      <MediaPlayer url={mediaUrl} />
     </>
   )
 }
