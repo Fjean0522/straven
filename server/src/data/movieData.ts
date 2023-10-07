428812d0651838313cf58634bf96f1679a5b2bc0
Fjean <fjean0522@gmail.com>
2023-10-06 20:05:31 -0400

    Setup movie data skeleton.

--- server/src/data/movieData.ts
+++ server/src/data/movieData.ts
@@ -1,22 +1,186 @@
 import { Movie } from "../models/Movie";
 
 const movieData: Movie[] = [
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
 
-]
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+
+  new Movie({
+    title: '',
+    description: '',
+    category: '',
+    duration: '',
+    imageUrl: '',
+    videoUrl: ''
+  }),
+];
+
 
 export default movieData;
-
-
-
-
-/* const moviesToAdd = [
-  new Movie({
-    title: 'Movie 1',
-    releaseYear: 2022,
-  }),
-  new Movie({
-    title: 'Movie 2',
-    releaseYear: 2023,
-  }),
-  // Add more movie instances as needed
-]; */
