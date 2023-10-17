054a3d4c9d2a7b343cf4faa9372187d0568acc84
Fjean <fjean0522@gmail.com>
2023-10-16 20:37:08 -0400

    Create skeleton for movie routes.

---
+++ server/src/controllers/movieController.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+import { Movie } from "../models/Movie";
+
+
+export const getAllMovies = async (req: Request, res: Response) => {
+    
+};
+
+export const getMoviesByCategory = async (req: Request, res: Response) => {
+    
+}
+
+export const getMovie = async (req: Request, res: Response) => {
+    
+}
