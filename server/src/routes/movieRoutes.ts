81c90f33876c9ce976dd7312c2a95701ab3d910c
Fjean <fjean0522@gmail.com>
2023-10-16 20:37:50 -0400

    Get routes.

--- server/src/routes/movieRoutes.ts
+++ server/src/routes/movieRoutes.ts
@@ -1,3 +1,11 @@
+import express, { Router } from 'express';
+import { getAllMovies, getMoviesByCategory, getMovie } from '../controllers/movieController';
 
 
+const router: Router = express.Router();
 
+router.get('/', getAllMovies);
+router.get('/:movieId', getMovie);
+router.get('/category/:category', getMoviesByCategory);
+
+export default router;
