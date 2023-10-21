a75c446a78ddef4b1d298c243e2b4a09e42be602
Fjean <fjean0522@gmail.com>
2023-10-21 19:38:49 -0400

    Scale header logo on hover.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = ({ bgOpacityClass }: BgHeader) => {
         className={`sticky top-0 z-50 bg-black ${bgOpacityClass} flex justify-between items-center p-5 md:px-10 lg:px-14 `}>
         <div className='flex items-center gap-5'>
           <Link to='/home'>
-            <h1 className='text-white text-3xl font-bold cursor-pointer'>
+            <h1 className='text-white text-3xl font-bold cursor-pointer hover:scale-110'>
               <span className='text-blue-600'>Str</span>
               aven
             </h1>
