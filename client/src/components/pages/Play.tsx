ae8e0c4609ff4b499051db6287bbd3957534b03a
Fjean <fjean0522@gmail.com>
2023-10-21 17:14:38 -0400

    Fetch movie by title & assign videoUrl to url prop
    Title comes from the movie params (/play/movieTitle)

--- client/src/components/pages/Play.tsx
+++ client/src/components/pages/Play.tsx
@@ -1,16 +1,41 @@
+import { useState, useEffect } from "react"
+import { Movie } from "./Home"
+import { useParams } from "react-router-dom"
 import Header from "../Header"
 import MediaPlayer from '../MediaPlayer'
 
+const mediaDefault = {
+  _id: '',
+  title: '',
+  description: '',
+  category: '',
+  duration: '',
+  rating: '',
+  imageUrl: '',
+  videoUrl: ''
+}
+
 const Play = () => {
+  const [selectedMovie, setSelectedMovie] = useState<string | undefined>('')
+  const [media, setMedia] = useState<Movie>(mediaDefault)
 
-  const mediaUrl = 'https://storage.googleapis.com/straven_movies/movies/action/Black%20Adam.mp4'
+  const { title } = useParams<string>()
+  
+  useEffect(() => {
+    setSelectedMovie(title)
+    if (selectedMovie) {
+      fetch(`/api/movies/${selectedMovie}`)
+      .then(response => response.json())
+      .then(data => setMedia(data[0]))
+      .catch(error => console.log(error))
+    }
+  }, [selectedMovie, title])
 
-  console.log(`Media is displaying with url: ${mediaUrl}`)
-
+  
   return (
     <>
       <Header />
-      <MediaPlayer url={mediaUrl} />
+      <MediaPlayer url={media.videoUrl} media={media}/>
     </>
   )
 }
