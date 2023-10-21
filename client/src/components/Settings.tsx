277f3811b4e656d780d93859831ff9a3022ed4a8
Fjean <fjean0522@gmail.com>
2023-10-21 19:55:54 -0400

    Create Settings component.

---
+++ client/src/components/Settings.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+
+const Settings = () => {
+  return (
+    <>
+      <Header />
+      <div className="bg-black h-screen text-white flex flex-col items-center">
+          <h1 className="font-bold text-2xl pt-10 ">Settings</h1>
+      </div>
+    </>
+  )
+}
+
+export default Settings
