98d89a04c2c704d8eb1dee04149943f382e403f4
Fjean <fjean0522@gmail.com>
2023-10-21 19:41:18 -0400

    Scale category selectors on hover.

--- client/src/components/CategorySelector.tsx
+++ client/src/components/CategorySelector.tsx
@@ -6,28 +6,28 @@ const CategorySelector = () => {
     <div className="text-white font-bold flex justify-between p-6 md:justify-evenly md:text-lg md:mb-5">
         <Link 
           to={'/category/Action'}
-          className="hover:text-blue-600 cursor-pointer"
+          className="hover:text-blue-600 cursor-pointer hover:scale-105"
           >
           Action
         </Link>
 
         <Link 
           to={'/category/Comedy'}
-          className="hover:text-blue-600 cursor-pointer"
+          className="hover:text-blue-600 cursor-pointer hover:scale-105"
           >
           Comedy
         </Link>
 
         <Link 
           to={'/category/Thriller'}
-          className="hover:text-blue-600 cursor-pointer"
+          className="hover:text-blue-600 cursor-pointer hover:scale-105"
           >
           Thriller
         </Link>
 
         <Link 
           to={'/category/Horror'}
-          className="hover:text-blue-600 cursor-pointer"
+          className="hover:text-blue-600 cursor-pointer hover:scale-105"
           >
           Horror
         </Link>
