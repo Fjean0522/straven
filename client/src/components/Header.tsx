206e2718a7aa1bbda5fc8ac42b2c009b2f67a3b4
Fjean <fjean0522@gmail.com>
2023-10-21 19:25:20 -0400

    Update header logo link.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = ({ bgOpacityClass }: BgHeader) => {
       <nav 
         className={`sticky top-0 z-50 bg-black ${bgOpacityClass} flex justify-between items-center p-5 md:px-10 lg:px-14 `}>
         <div className='flex items-center gap-5'>
-          <Link to='/'>
+          <Link to='/home'>
             <h1 className='text-white text-3xl font-bold cursor-pointer'>
               <span className='text-blue-600'>Str</span>
               aven
