982d13d1abfafcaf1196be311ffef977da1f3b4e
Fjean <fjean0522@gmail.com>
2023-10-23 19:20:40 -0400

    Fetch a movie when user searches movie by name.

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { mediaDefault } from "./Play"
 import Header from "../Header"
 import Hero from "../Hero"
 import CategorySelector from "../CategorySelector"
@@ -18,34 +19,43 @@ export type Movie = {
 
 const Home = () => {
   const [movies, setMovies] = useState<Movie[]>([])
+  const [singleMovie, setSingleMovie] = useState<Movie>(mediaDefault)
+
 
   // Get url endpoints
-  const { home } = useParams<string>()
   const { movieCategory } = useParams<string>()
+  const { title } = useParams<string>()
 
   useEffect(() => {
-    // Fetch all movies if no category is specified in url or when home is in url
-    if (home || !movieCategory) {
-      fetch('/api/movies')
+    // Fetch a single movie when user searches for movie by name
+    if (title) {
+        fetch(`/api/movies/${title}`)
         .then(response => response.json())
-        .then(data => setMovies(data))
+        .then(data => setSingleMovie(data[0]))
         .catch(error => console.log(error))
 
     // Fetch movies by category if category is specified in url 
     } else if (movieCategory) {
-        fetch(`/api/movies/category/${movieCategory}`)
+      fetch(`/api/movies/category/${movieCategory}`)
+        .then(response => response.json())
+        .then(data => setMovies(data))
+        .catch(error => console.log(error))
+
+    // Fetch all movies if no title or category is specified
+    } else if (!movieCategory || !title) {
+        fetch('/api/movies')
         .then(response => response.json())
         .then(data => setMovies(data))
         .catch(error => console.log(error))
     }
-  }, [home, movieCategory])
+  }, [movieCategory, title])
 
   return (
     <div className="bg-black min-h-screen">
       <Header bgOpacityClass="bg-opacity-50" />
       <Hero />
       <CategorySelector />
-      <MediaDisplay movies={movies} />
+      <MediaDisplay movies={movies} singleMovie={singleMovie}/>
     </div>
   )
 }
