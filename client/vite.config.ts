4cea7137baa58b4cd124768a06d01a93036a1b78
Fjean <fjean0522@gmail.com>
2023-10-25 20:22:58 -0400

    Comment out proxy server in vite config file.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,11 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: /\.(mp4|webm)$/,
-  // base: '/',
-  server: {
+  /* server: {
     proxy: {
       // proxy the api address
       '/api': 'http://localhost:3000'
     }
-  }
+  } */
 })
