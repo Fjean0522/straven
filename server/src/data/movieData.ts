56e5c245b96320c14e308d10805a9130be2bcc5e
Fjean <fjean0522@gmail.com>
2023-11-03 18:44:26 -0400

    Add movies to db & remove from array.
    Add to data storage backup.

--- server/src/data/movieData.ts
+++ server/src/data/movieData.ts
@@ -2,114 +2,16 @@ import { Movie } from "../models/Movie";
 
 const movieData: Movie[] = [
   {
-  title: 'Blacklight',
-  description: `Travis Block is a government operative coming to terms with his shadowy past. 
-  When he discovers a plot targeting U.S. citizens, Block finds himself in the crosshairs of 
-  the FBI director he once helped protect.`,
-  category: 'Action',
-  duration: '1h 44m',
-  rating: 'PG-13',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/action/blacklight.jpeg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/action/Blacklight.mp4'
-  },
+  title: '',
+  description: ``,
+  category: '',
+  duration: 'h m',
+  rating: '',
+  imageUrl: '',
+  videoUrl: ''
+},
 
-  {
-  title: 'Black Panther',
-  description: `T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people
-  into a new future and must confront a challenger from his country's past.`,
-  category: 'Action',
-  duration: '2h 14m',
-  rating: 'PG-13',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/action/black_panther.jpg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/action/Black%20Panther.mp4'
-  },
 
-  {
-  title: 'Sleepless',
-  description: `A cop with a connection to the criminal underworld scours a nightclub in search of his kidnapped son.`,
-  category: 'Action',
-  duration: '1h 35m',
-  rating: 'R',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/action/sleepless.jpg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/action/Sleepless.mp4'
-  },
-
-  {
-  title: 'Wanted',
-  description: `A frustrated office worker discovers that he is the son of a professional assassin, 
-  and that he shares his father's superhuman killing abilities.`,
-  category: 'Action',
-  duration: '1h 50m',
-  rating: 'R',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/action/wanted.jpeg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/action/Wanted.mp4'
-  },
-
-  {
-  title: 'No Exit',
-  description: `During a blizzard and stranded at an isolated highway rest stop in the mountains, 
-  a college student discovers a kidnapped child hidden in a car belonging to one of the people inside.`,
-  category: 'Thriller',
-  duration: '1h 35m',
-  rating: 'R',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/thriller/no_exit.jpeg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/thriller/No%20Exit.mp4'
-  },
-
-  {
-  title: 'The Guilty',
-  description: `A demoted police officer assigned to a call dispatch desk is conflicted when he receives 
-  an emergency phone call from a kidnapped woman.`,
-  category: 'Thriller',
-  duration: '1h 29m',
-  rating: 'R',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/thriller/the_guilty.jpeg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/thriller/The%20Guilty.mp4'
-  },
-
-  {
-  title: 'The Purge: Anarchy',
-  description: `Three groups of people intertwine and are left stranded in the streets on Purge Night, 
-  trying to survive the chaos and violence that occurs.`,
-  category: 'Thriller',
-  duration: '1h 43m',
-  rating: 'R',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/thriller/the_purge_anarchy.jpeg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/thriller/The%20Purge%20Anarchy.mp4'
-  },
-
-  {
-  title: 'The Strangers',
-  description: `A young couple staying in an isolated vacation home are terrorized by three unknown assailants.`,
-  category: 'Thriller',
-  duration: '1h 27m',
-  rating: 'R',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/thriller/the_strangers.jpg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/thriller/The%20Strangers.mp4'
-  },
-
-  {
-  title: 'Ma',
-  description: `A lonely woman befriends a group of teenagers and decides to let them party at her house. 
-  Just when the kids think their luck couldn't get any better, things start happening that make them question 
-  the intention of their host.`,
-  category: 'Thriller',
-  duration: '1h 39m',
-  rating: 'R',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/thriller/ma.jpg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/thriller/Ma.mp4'
-  },
-
-  {
-  title: 'Wrong Turn',
-  description: `Chris and a group of five friends are left stranded deep in the middle of the woods after their cars collide. 
-  As they venture deeper into the woods, they face an uncertain and bloodcurdling fate.`,
-  category: 'Thriller',
-  duration: '1h 24m',
-  rating: 'R',
-  imageUrl: 'https://storage.googleapis.com/straven_movies/movie_thumbnails/horror/wrong_turn.jpg',
-  videoUrl: 'https://storage.googleapis.com/straven_movies/movies/thriller/Wrong%20Turn.mp4'
-  },
 ];
 
 export default movieData;
