a90a62b87edfc5298a036aa8bd2d89876135d1d9
Fjean <fjean0522@gmail.com>
2023-11-08 20:17:43 -0400

    Create route to get a random movie.

--- server/src/controllers/movieController.ts
+++ server/src/controllers/movieController.ts
@@ -5,6 +5,7 @@ import { Movie } from "../models/Movie";
 // GET all movies
 export const getAllMovies = async (req: Request, res: Response) => {
     try {
+        // get movies in random order
         const movies = await Movie.aggregate(
             [ { $sample: { size: 30 } } ]
         );
@@ -37,5 +38,20 @@ export const getMovie = async (req: Request, res: Response) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Server Error: Unable to fetch movie' });
+    };
+};
+
+
+// GET a single movie randomly
+export const getHeroMovie = async (req: Request, res: Response) => {
+    try {
+        const randomMovie = await Movie.aggregate(
+            [ { $sample: { size: 1 } } ]
+        );
+
+        res.status(200).json(randomMovie);
+    } catch (error) {       
+        console.log(error);
+        res.status(500).json({ message: 'Server Error: Unable to fetch movies' });
     };
 };
--- server/src/routes/movieRoutes.ts
+++ server/src/routes/movieRoutes.ts
@@ -1,14 +1,17 @@
 import express, { Router } from 'express';
-import { getAllMovies, getMoviesByCategory, getMovie } from '../controllers/movieController';
+import { getAllMovies, getMoviesByCategory, getMovie, getHeroMovie } from '../controllers/movieController';
 
 
 const router: Router = express.Router();
 
 // GET all movies
-router.get('/', getAllMovies)
+router.get('/', getAllMovies);
 
 // GET all movies of a specific category
 router.get('/category/:category', getMoviesByCategory);
+
+// GET a single movie randomly
+router.get('/heroMovie', getHeroMovie);
 
 // GET a single movie
 router.get('/:title', getMovie);
