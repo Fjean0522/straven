0cdc1bf9057da4ee1a62efbef099fc985cb8e55c
Fjean <fjean0522@gmail.com>
2023-11-30 14:15:06 -0400

    Create auth route protection middleware.

---
+++ server/src/middleware/authMiddleware.ts
@@ -0,0 +1,30 @@
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
+import User from '../models/user';
+import { Request, Response, NextFunction } from 'express';
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        user?: JwtPayload;
+    }
+}
+
+export const protect = async (req: Request, res: Response, next: NextFunction) => {
+    let token: string | undefined;
+    
+    token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET as Secret) as JwtPayload;
+
+            req.user = await User.findById(decoded.userId).select('-password');
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorized, invalid token');
+        };
+    } else {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    };
+};
