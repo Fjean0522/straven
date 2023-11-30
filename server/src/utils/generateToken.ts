fdbac2b879b59340ba6332aa99a34f9d33daaabe
Fjean <fjean0522@gmail.com>
2023-11-30 15:31:37 -0400

    Update generateToken function userId param type.

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -1,8 +1,9 @@
 import jwt, { Secret } from 'jsonwebtoken';
 import { Response } from 'express';
+import { Types } from 'mongoose';
 
 // Create json web token & configure cookie
-const generateToken = (res: Response, userId: string) => {
+const generateToken = (res: Response, userId: Types.ObjectId) => {
     const daysToExpire: number = 15;
 
     const token = jwt.sign({ userId }, process.env.JWT_SECRET as Secret, { expiresIn: '15d' });
