2588b16b2a2cdb47e393e6844ca08500887fd1e3
Fjean <fjean0522@gmail.com>
2023-11-30 15:15:26 -0400

    Define userLogout route.

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -72,7 +72,12 @@ const loginUser = async (req: Request, res: Response) => {
 // Route:  POST /api/users/logout 
 // Access: Public
 const logoutUser = async (req: Request, res: Response) => {
-    
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+
+    res.status(200).json({ message: 'User has Loged out' });
 };
 
 
