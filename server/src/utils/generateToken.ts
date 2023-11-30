e7c5f96427a69c683f28fc13c3d132186137bb9d
Fjean <fjean0522@gmail.com>
2023-11-29 20:21:16 -0400

    Update generateToken function.

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -3,13 +3,15 @@ import { Response } from 'express';
 
 // Create json web token & configure cookie
 const generateToken = (res: Response, userId: string) => {
+    const daysToExpire: number = 15;
+
     const token = jwt.sign({ userId }, process.env.JWT_SECRET as Secret, { expiresIn: '15d' });
 
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development', // Sets to true in production for secure cookies (HTTPS)
         sameSite: 'none',
-        maxAge: 1296000000  // 15 days in milliseconds
+        maxAge: daysToExpire * 24 * 60 * 60 * 1000  
     });
 };
 
