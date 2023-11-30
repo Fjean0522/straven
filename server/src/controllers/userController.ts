2204cd37613816c75fd9228160517d24970cb3aa
Fjean <fjean0522@gmail.com>
2023-11-30 15:12:18 -0400

    Define loginUser route.

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -49,7 +49,22 @@ const registerUser = async (req: Request, res: Response) => {
 // Route:  POST /api/users/login 
 // Access: Public
 const loginUser = async (req: Request, res: Response) => {
-    
+    const { email, password } = req.body;
+
+    //  Check if user is registered in the database
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    };
 };
 
 
