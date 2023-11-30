b07ad0fd6788d7f680c1437e890a12d9ff327172
Fjean <fjean0522@gmail.com>
2023-11-30 14:50:15 -0400

    Define registerUser route.

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,18 +1,54 @@
+import { log } from "console";
+import User from "../models/user";
+import generateToken from "../utils/generateToken";
 import { Request, Response } from "express"
 
 
 // Task:   Register new user
 // Route:  POST /api/users 
 // Access: Public
-const registerUser = (req: Request, res: Response) => {
+const registerUser = async (req: Request, res: Response) => {
+    try {
+        const { name, email, password } = req.body;
     
+        // Check if user already exists
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            res.status(400);
+            throw new Error('User already exists');
+        };
+
+        // Else create new user
+        const user = await User.create({
+            name,
+            email,
+            password
+        });
+
+        // Generate token if user was successfully created
+        if (user) {
+            generateToken(res, user._id);
+            res.status(201).json({
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            });
+        } else {
+            res.status(400);
+            throw new Error('Invalid user data');
+        };
+
+    } catch (error) {
+        console.log(error);
+    };
 };
 
 
 // Task:   Authenticate / Login user
 // Route:  POST /api/users/login 
 // Access: Public
-const loginUser = (req: Request, res: Response) => {
+const loginUser = async (req: Request, res: Response) => {
     
 };
 
@@ -20,7 +56,7 @@ const loginUser = (req: Request, res: Response) => {
 // Task:   Authenticate / Login user
 // Route:  POST /api/users/logout 
 // Access: Public
-const logoutUser = (req: Request, res: Response) => {
+const logoutUser = async (req: Request, res: Response) => {
     
 };
 
@@ -28,7 +64,7 @@ const logoutUser = (req: Request, res: Response) => {
 // Task:   Get user profile
 // Route:  GET /api/users/profile 
 // Access: Private
-const getUserProfile = (req: Request, res: Response) => {
+const getUserProfile = async (req: Request, res: Response) => {
     
 };
 
@@ -36,7 +72,7 @@ const getUserProfile = (req: Request, res: Response) => {
 // Task:   Update user profile
 // Route:  PUT /api/users/profile
 // Access: Private
-const updateUserProfile = (req: Request, res: Response) => {
+const updateUserProfile = async (req: Request, res: Response) => {
     
 };
 
