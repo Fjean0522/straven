849d833cf8f709201e52570174ec1177e8fed6be
Fjean <fjean0522@gmail.com>
2023-12-01 16:12:27 -0400

    Add protection middleware to movie routes.

--- server/src/routes/movieRoutes.ts
+++ server/src/routes/movieRoutes.ts
@@ -1,19 +1,26 @@
 import express, { Router } from 'express';
-import { getAllMovies, getMoviesByCategory, getMovie, getRandomMovie } from '../controllers/movieController';
+import { protect } from '../middleware/authMiddleware';
+import { 
+    getAllMovies, 
+    getMoviesByCategory, 
+    getRandomMovie ,
+    getMovie
+} from '../controllers/movieController';
 
 
 const router: Router = express.Router();
 
 // GET all movies
-router.get('/', getAllMovies);
+router.get('/', protect, getAllMovies);
 
 // GET all movies of a specific category
-router.get('/category/:category', getMoviesByCategory);
+router.get('/category/:category', protect, getMoviesByCategory);
 
 // GET a single movie randomly
-router.get('/randomMovie', getRandomMovie);
+router.get('/randomMovie', protect, getRandomMovie);
 
 // GET a single movie
-router.get('/:title', getMovie);
+router.get('/:title',protect, getMovie);
+
 
 export default router;
