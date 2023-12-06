b4bfb19d49084b0c8166c177c7aff31879865632
Fjean <fjean0522@gmail.com>
2023-12-06 14:55:47 -0500

    Update routes & configure private routes.

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,23 +1,42 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home' 
 import Play from './pages/Play'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+
+const userAuthenticated = localStorage.getItem('user')
 
 const Routes = () => {
     const routes = createBrowserRouter([
         {
+            path: '/signup',
+            element: <Signup />
+        },
+
+        {
+            path: 'login',
+            element: <Login />
+        },
+
+        {
+            index: true,
+            element: userAuthenticated ? <Home /> : <Login />
+        },
+
+        {
             path: '/',
-            element: <Home />
+            element: userAuthenticated ? <Home /> : <Login />
         },
 
         {
             path: '/play/:title',
-            element: <Play />
+            element: userAuthenticated ? <Play /> : <Login />
         },
 
         {
             path: '/category/:movieCategory',
-            element: <Home />
-        },
+            element: userAuthenticated ? <Home /> : <Login />
+        }
     ])
 
   return <RouterProvider router={routes}/>
