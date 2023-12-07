732108ef449b202f25799c5f516e5bece537afd9
Fjean <fjean0522@gmail.com>
2023-12-07 16:54:39 -0500

    Add base style to settings page.

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,7 +1,13 @@
+import Header from "../components/Header"
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <>
+     <Header />
+     <div className="bg-black text-white text-3xl text-center pt-5 font-semibold min-h-screen">
+        Settings
+     </div> 
+    </>
   )
 }
 
