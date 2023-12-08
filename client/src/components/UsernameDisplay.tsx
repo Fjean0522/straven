ed997c9af0f4b9d699c3e53909b0f79936cddb73
Fjean <fjean0522@gmail.com>
2023-12-08 17:04:42 -0500

    Get user profile to display username when loged in

--- client/src/components/UsernameDisplay.tsx
+++ client/src/components/UsernameDisplay.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react"
+import { API_URL } from "../pages/Home"
 
 const UsernameDisplay = () => {
-  
+  const [user, setUser] = useState()
+
+  const getUser = async () => {
+    try {
+      const response = await fetch(`${API_URL}/users/profile`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: "include"
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        setUser(data.username)
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [user])
 
   return (
     <div className="text-white font-semibold md:text-lg">
-     Username
+     {user}
     </div>
   )
 }
