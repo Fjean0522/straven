7737b1a23e9ffdcbcf1c09fdd7be2da68e303689
Fjean <fjean0522@gmail.com>
2023-12-15 14:59:21 -0500

    Make WatchList movie display responsive.

--- client/src/pages/WatchList.tsx
+++ client/src/pages/WatchList.tsx
@@ -50,23 +50,23 @@ const WatchList = () => {
           :
           watchlist.length > 0 ? (
             <div 
-              className='flex flex-wrap pt-2 pb-5 md:px-7'
+              className='flex flex-wrap justify-center pt-2 pb-5 w-full sm:px-4 md:px-5'
             >
               {
                 watchlist.map(movie => (
-                  <Link 
-                    to={`/play/${movie.title}`}
-                    className='text-white font-semibold text-base flex flex-col items-center gap-2 h-full mx-1 mt-6
-                    hover:cursor-pointer hover:-translate-y-2 transition-transform duration-300 ease-in md:mt-0'
-                    key={movie._id} 
-                    >
-                    <img 
-                      className='hover:border hover:border-blue-400 rounded-md h-full w-2/3 '
-                      src={movie.imageUrl} 
-                      alt={movie.title + 'Thumbnail'} 
-                    />
-                    <h1>{movie.title}</h1>
-                  </Link>
+                    <Link 
+                      to={`/play/${movie.title}`}
+                      className='text-white font-semibold text-base flex flex-col items-center gap-3 md:w-2/6 lg:w-2/6 mx-1 mt-6 
+                      hover:cursor-pointer md:mt-0 '
+                      key={movie._id} 
+                      >
+                      <img 
+                        className='rounded-md mt-16 hover:border hover:border-blue-400'
+                        src={movie.imageUrl} 
+                        alt={movie.title + 'Thumbnail'} 
+                      />
+                      <h1>{movie.title}</h1>
+                    </Link>
                 ))
               }
             </div>
